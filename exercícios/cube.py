8734aa78aa05854606aa28997c01c2f6eb247b4e
figenio <matigenioo@gmail.com>
2021-04-01 12:09:08 -0300

    Implementando transformações com o cubo

--- exercícios/cube.py
+++ exercícios/cube.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
 
-## @file transform.py
-#  Applies geometric transformations to a rectangle.
-# 
-# Iteratively rotates a rectangle using two modes acording to keyboard key:
-# 1 for around the center of the object;
-# 2 for around coordinate system origin.
-#
-# @author Ricardo Dutra da Silva
-
-
 import sys
 import ctypes
 import numpy as np
@@ -75,27 +65,13 @@ void main()
 #
 # Draws primitive.
 def display():
+    global M
 
     gl.glClearColor(0.2, 0.3, 0.3, 1.0)
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
     gl.glUseProgram(program)
     gl.glBindVertexArray(VAO)
-
-    # Translation. 
-    #T  = ut.matTranslate(0.5, -0.5, 0.0)
-    # Rotation around z-axis.
-    #Rz = ut.matRotateZ(np.radians(angle))
-    # Scale.
-    #S  = ut.matScale(0.3, 0.5, 0.0)
-
-    global M
-    #if mode == 1:
-    #    M = np.matmul(Rz,S)
-    #    M = np.matmul(T,M)
-    #elif mode == 2:
-    #    M = np.matmul(T,S)
-    #    M = np.matmul(Rz,M)
 
     # Retrieve location of tranform variable in shader.
     loc = gl.glGetUniformLocation(program, "transform")
@@ -103,7 +79,7 @@ def display():
     gl.glUniformMatrix4fv(loc, 1, gl.GL_FALSE, M.transpose())
 
     #gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
-    gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, None)
+    gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, None)
 
     glut.glutSwapBuffers()
 
@@ -115,10 +91,9 @@ def display():
 # @param width New window width.
 # @param height New window height.
 def reshape(width,height):
-
     win_width = width
     win_height = height
-    gl.glViewport(0, 0, width, height)
+    gl.glViewport(0, 0, win_width, win_height)
     glut.glutPostRedisplay()
 
 
@@ -134,22 +109,105 @@ def keyboard(key, x, y):
     global type_primitive
     global mode
 
-    if key == b'\x1b'or key == b'q':
+    # Primeiro verifica modo de exibição
+    if key == b'\x1b':
         sys.exit( )
     if key == b'1':
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE) # Linhas
         mode = 1
     if key == b'2':
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) # Preenchido
         mode = 2
+    if key == b'3':
+        mode = 3 # Rotação X constante
+    if key == b'4':
+        mode = 4 # Rotação Y constante
+    if key == b'5':
+        mode = 5 # Rotação Z constante
+
+    # TRANSFORMAÇÕES
+    #
+    # Translação
+    if key == b'a':   # Esquerda
+        translation(-0.05, 0.0, 0.0)
+    if key == b'd':   # Direita
+        translation(0.05, 0.0, 0.0)
+    if key == b'w':   # Cima
+        translation(0.0, 0.05, 0.0)
+    if key == b's':   # Baixo
+        translation(0.0, -0.05, 0.0)
+
+    # Escalando
+    if key == b'u': # Aumentar largura
+        scaling(1.05, 1.00, 1.00)
+    if key == b'i': # Aumentar altura
+        scaling(1.00, 1.05, 1.00)
+    if key == b'o': # Aumentar profundidade
+        scaling(1.0, 1.00, 1.05)
+    if key == b'j': # Diminuir largura
+        scaling(0.95, 1.00, 1.00)
+    if key == b'k': # Diminuir largura
+        scaling(1.00, 0.95, 1.00)
+    if key == b'l': # Diminuir largura
+        scaling(1.00, 1.00, 0.95)
+    if key == b'+': # Aumentar TUDO
+        scaling(1.05, 1.05, 1.05)
+    if key == b'-': # Diminuir TUDO
+        scaling(1.05, 1.05, 1.05)
+    
+    # Rotação
+    if key == b'x':
+        rotation('x')
+    if key == b'y':
+        rotation('y')
+    if key == b'z':
+        rotation('z')
+    
 
     glut.glutPostRedisplay()
+
+
+# Método que caclula a translação do cubo em e, y e z
+def translation(x, y, z):
+    global M
+
+    T = ut.matTranslate(x, y, z) # Configura a matriz de translação com os valores
+    M = np.matmul(T,M) # Multiplica a matriz T pela "geral" M
+    glut.glutPostRedisplay() # Chama a redisplay para redesenhar
+
+# Método que calcula a escala do cubo em x, y e z
+def scaling(x, y, z):
+    global M
+
+    S = ut.matScale(x, y, z) # Gera a matriz de scala com os valores
+    M = np.matmul(S,M) # Multiplica a matriz S pela "geral" M
+    glut.glutPostRedisplay() # Chama a redisplay para redesenhar
+
+def rotation(axis, angle=10.0):
+    global M
+
+    if axis == 'x':
+        R = ut.matRotateX(np.radians(angle))
+    if axis == 'y':
+        R = ut.matRotateY(np.radians(angle))
+    if axis == 'z':
+        R = ut.matRotateZ(np.radians(angle))
+
+    M = np.matmul(R,M)
+    glut.glutPostRedisplay()
+
+
+
 
 ## Idle function.
 #
 # Called continuously.
+# idle foi definida para continuar a incrementar o ângulo em determinado modo de execição do programa
 def idle():
     global angle
 
-    angle = angle+angle_inc if (angle+angle_inc) < 360.0 else (360.0-angle+angle_inc)
+    if mode == 3:
+        angle = angle+angle_inc if (angle+angle_inc) < 360.0 else (360.0-angle+angle_inc)
 
     glut.glutPostRedisplay()
 
@@ -163,25 +221,25 @@ def initData():
     global VAO
     global VBO
 
-    # Set vertices.
+    # Set cube vertices.
     vertices = np.array([
-        # coordinate     color
-        -0.5, -0.5, -0.5, 1.0, 0.0, 0.0, # 0
-        -0.5, -0.5,  0.5, 0.0, 0.0, 1.0, # 1
-        -0.5,  0.5, -0.5, 1.0, 0.0, 0.0, # 2
-        -0.5,  0.5,  0.5, 0.0, 0.0, 1.0, # 3
-         0.5, -0.5, -0.5, 1.0, 0.0, 0.0, # 4
-         0.5, -0.5,  0.5, 0.0, 0.0, 1.0, # 5
-         0.5,  0.5, -0.5, 1.0, 0.0, 0.0, # 6
-         0.5,  0.5,  0.5, 0.0, 0.0, 1.0  # 7
+        # coordinate        color
+        -0.5, -0.5, -0.5,   0.58, 0.92, 0.86, # 0
+        -0.5, -0.5,  0.5,   0.48, 0.80, 0.83, # 1
+        -0.5,  0.5, -0.5,   0.48, 0.83, 0.66, # 2
+        -0.5,  0.5,  0.5,   0.56, 0.96, 1.00, # 3
+         0.5, -0.5, -0.5,   0.58, 0.92, 0.86, # 4
+         0.5, -0.5,  0.5,   0.48, 0.80, 0.83, # 5
+         0.5,  0.5, -0.5,   0.48, 0.83, 0.66, # 6
+         0.5,  0.5,  0.5,   0.56, 0.96, 1.00  # 7
     ], dtype='float32')
     
-    # Set indixes
+    # Set cube faces with indexes
     indices = np.array([
         1, 3, 5, 7, 3, 5, # Face Front
         1, 0, 3, 2, 0, 3, # Face Left
         0, 4, 2, 6, 4, 2, # Face Back
-        4, 5, 7, 6, 5, 7, # Face Right
+        5, 4, 7, 6, 4, 7, # Face Right
         3, 2, 7, 6, 2, 7, # Face Up
         1, 0, 5, 4, 0, 5  # Face Down
     ], dtype="uint32")
@@ -205,6 +263,10 @@ def initData():
     gl.glEnableVertexAttribArray(0)
     gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 6*vertices.itemsize, c_void_p(3*vertices.itemsize))
     gl.glEnableVertexAttribArray(1)
+
+    # Enables depht test and sets it
+    gl.glEnable(gl.GL_DEPTH_TEST)
+    gl.glDepthFunc(gl.GL_LESS)
     
     # Unbind Vertex Array Object.
     gl.glBindVertexArray(0)
@@ -240,7 +302,7 @@ def main():
     glut.glutReshapeFunc(reshape)
     glut.glutDisplayFunc(display)
     glut.glutKeyboardFunc(keyboard)
-    #glut.glutIdleFunc(idle)
+    glut.glutIdleFunc(idle)
 
     glut.glutMainLoop()
 
